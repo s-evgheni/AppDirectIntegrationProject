d103642a0e6aa364676496134877f4bb2905152d
s-evgheni <sadovoi@gmail.com>
2015-09-08 01:57:36 -0600

    Added basic openID integration (part 2)

---
+++ grails-app/domain/com/test/auth/PersistentLogin.groovy
@@ -0,0 +1,41 @@
+package com.test.auth
+
+import groovy.transform.ToString
+
+import org.apache.commons.lang.builder.HashCodeBuilder
+
+@ToString(cache=true, includeNames=true, includePackage=false)
+class PersistentLogin implements Serializable {
+
+	private static final long serialVersionUID = 1
+
+	String series
+	String username
+	String token
+	Date lastUsed
+
+	@Override
+	int hashCode() {
+		new HashCodeBuilder().append(series).append(username).toHashCode()
+	}
+
+	@Override
+	boolean equals(other) {
+		is(other) || (
+			other instanceof PersistentLogin &&
+			other.series == series &&
+			other.username == username)
+	}
+
+	static constraints = {
+		series maxSize: 64
+		token maxSize: 64
+		username maxSize: 64
+	}
+
+	static mapping = {
+		table 'persistent_login'
+		id name: 'series', generator: 'assigned'
+		version false
+	}
+}
